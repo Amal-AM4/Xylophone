40b922ac830f7e214383c6bd6d0b6411fb8268f0
Amal A M <vectorhero007@gmail.com>
2025-06-02 10:41:18 +0530

    first note is added and disable the tapsound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
@@ -16,13 +17,78 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextButton(
-                  style: TextButton.styleFrom(backgroundColor: Colors.red),
-                  onPressed: () {
-                    print('btn is clicked');
-                  },
-                  child: Text(''),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      enableFeedback: false,
+                    ),
+                    onPressed: () async {
+                      final player = AudioPlayer();
+                      await player.play(AssetSource('sound/note-1_1.mp3'));
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green.shade400,
+                    ),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.purple.shade800,
+                    ),
+                    onPressed: () {
+                      print('btn is clicked');
+                    },
+                    child: Text(''),
+                  ),
                 ),
               ],
             ),
